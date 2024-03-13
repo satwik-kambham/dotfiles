f9cf39174eec124bddceae83f040b9a20a62872f
Satwik Kambham <satwik.kambham@gmail.com>
2024-03-13 10:24:34 +0530

    Added new configuraion based on hyprland

--- configuration-old.nix
+++ configuration-old.nix
@@ -1,8 +1,12 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
 { config, pkgs, ... }:
 
 {
   imports =
-    [
+    [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
     ];
 
@@ -10,7 +14,12 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  networking.hostName = "satwik-nixos";
+  networking.hostName = "satwik-nixos"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
   # Enable networking
   networking.networkmanager.enable = true;
@@ -33,34 +42,34 @@
     LC_TIME = "en_IN";
   };
 
-  # Configure keymap in X11
-  services.xserver = {
-    layout = "us";
-    xkbVariant = "";
-  };
-
-  # Desktop managers, drivers, window managers, etc.
-
-  # Enable display manager
-  services.xserver.displayManager.gdm.enable = true;
-
-  # Enable X11 windowing system
+  # Enable the X11 windowing system.
   services.xserver.enable = true;
+
+  # Enable the GNOME Desktop Environment.
+  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.desktopManager.gnome.enable = true;
 
   # Enable Hyprland wayland compositor
   programs.hyprland = {
     enable = true;
     xwayland.enable = true;
   };
-  security.polkit.enable = true;
 
-  # Environmental variables for hyprland to work well on nvidia hardware
+  # xdg.portal.enable = true;
+  # xdg.portal.extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
+
   environment.sessionVariables = {
     WLR_NO_HARDWARE_CURSORS = "1";
     NIXOS_OZONE_WL = "1";
   };
 
-  # GPU Drivers and other nvidia related configs
+  # Configure keymap in X11
+  services.xserver = {
+    layout = "us";
+    xkbVariant = "";
+  };
+  
+  # NVIDIA GPU stuff
   services.xserver.videoDrivers = [ "nvidia" ];
   hardware.opengl.extraPackages = with pkgs; [
     vaapiVdpau
@@ -81,16 +90,14 @@
         enableOffloadCmd = true;
       };
     };
-  };
+  }; 
   
   # Solution for heat (https://www.reddit.com/r/NixOS/comments/1712eoe/is_there_a_way_to_check_metricsheat_regarding_cpu/?rdt=44113)
   services.auto-cpufreq.enable = true;
 
-  # Bluetooth
-  hardware.bluetooth.enable = true;
-  hardware.bluetooth.powerOnBoot = true;
-  services.blueman.enable = true;
-  
+  # Enable CUPS to print documents.
+  services.printing.enable = true;
+
   # Enable sound with pipewire.
   sound.enable = true;
   hardware.pulseaudio.enable = false;
@@ -101,70 +108,56 @@
     alsa.support32Bit = true;
     pulse.enable = true;
     jack.enable = true;
+
+    # use the example session manager (no others are packaged yet so this is enabled by default,
+    # no need to redefine it in your config for now)
+    #media-session.enable = true;
   };
 
   # Enable touchpad support (enabled default in most desktopManager).
-  services.xserver.libinput.enable = true;
+  # services.xserver.libinput.enable = true;
 
-  # Defining user accounts. Don't forget to set a password with ‘passwd’.
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.defaultUserShell = pkgs.zsh;
   users.users.satwik = {
     isNormalUser = true;
     description = "satwik";
     extraGroups = [ "networkmanager" "wheel" ];
     packages = with pkgs; [
       firefox
-      spotify
     ];
     shell = pkgs.zsh;
   };
-  
-  # Opening port for syncing spotify
-  networking.firewall.allowedTCPPorts = [ 57621 ];
 
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
 
-  # Enabling experimental features
-  nix.settings.experimental-features = [ "nix-command" "flakes" ];
-
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
   environment.systemPackages = with pkgs; [
-    # Text Editor
-    helix
     neovim
+    alacritty
+    kitty
     vscode
 
-    # Development Tools
+    # Hyprland
+    eww-wayland # Status Bar
+    dunst # Notifications
+    swww # Wallpaper
+    rofi-wayland # Launcher
+    networkmanagerapplet
+    polkit_gnome # Authenication
+    
+    # Development tools
+    curl
+    busybox
     git
     gh
-    curl
-    wget
     ripgrep
     tmux
     eza
-    podman-tui
-    podman-desktop
-
-    # Terminal Emulators
-    alacritty
-    kitty
-
-    # Hyprland
-    waybar # Status Bar
-    dunst # Notification Manager
-    swww # Wallpapar Mamager
-    rofi-wayland # Launcher
-    networkmanagerapplet
-    brightnessctl # Brightness controller
-    hyprpicker # Color picker
-    cliphist # Clipboard Manager
   ];
-
-  # Containerization
-  virtualisation.podman = {
-    enable = true;
-    enableNvidia = true;
-  };
- 
+  
   # Fonts
   fonts.packages = with pkgs; [
     (nerdfonts.override { fonts = [ "FiraCode" ]; })
@@ -183,7 +176,7 @@
     shellAliases = {
       l = "eza -la --icons=auto";
       update = "sudo nixos-rebuild switch";
-      shx = "sudo hx";
+      svim = "sudo nvim";
     };
     
     histSize = 10000;
@@ -195,6 +188,31 @@
     };
   };
 
-  system.stateVersion = "23.11";
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "23.11"; # Did you read the comment?
 
 }
