e3ccb535d745beacc2472da0ab34f01d4ee05a75
Satwik Kambham <satwik.kambham@gmail.com>
2024-04-02 18:43:04 +0530

    Disable gnome and gdm

--- nixos/nixosModules/gnome.nix
+++ nixos/nixosModules/gnome.nix
@@ -1,6 +1,6 @@
 { pkgs, ... }:
 
 {
-  # services.xserver.displayManager.dgm.enable = true;
+  # services.xserver.displayManager.gdm.enable = true;
   services.xserver.desktopManager.gnome.enable = true;
 }
