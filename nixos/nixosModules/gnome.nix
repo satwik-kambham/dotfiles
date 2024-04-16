cd2ed212befe4042a4c99fbcdc62395c70d4fda4
Satwik Kambham <satwik.kambham@gmail.com>
2024-04-16 14:26:20 +0530

    Added pop shell to gnome

--- nixos/nixosModules/gnome.nix
+++ nixos/nixosModules/gnome.nix
@@ -3,4 +3,8 @@
 {
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.desktopManager.gnome.enable = true;
+
+  environment.systemPackages = with pkgs; [
+    gnomeExtensions.pop-shell
+  ];
 }
