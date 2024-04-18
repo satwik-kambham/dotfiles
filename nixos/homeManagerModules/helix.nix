186ae6b515c50dcb82e1ab1a13f3ee5cdff7701b
Satwik Kambham <satwik.kambham@gmail.com>
2024-04-18 21:33:45 +0530

    Using transparent terminal theme

--- nixos/homeManagerModules/helix.nix
+++ nixos/homeManagerModules/helix.nix
@@ -5,8 +5,15 @@
   programs.helix = {
     enable = true;
     settings = {
-      theme = "catppuccin_mocha";
+      theme = "fleet_dark_transparent";
       editor.soft-wrap.enable = true;
+    };
+    themes.fleet_dark_transparent = {
+      inherits = "fleet_dark";
+      "ui.background" = {};
+      "ui.statusline" = {
+        fg = "#d1d1d1";
+      };
     };
   };
 
