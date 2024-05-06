d0e3a4c846e2aeb43e3dbd7cee5258a5b6fc8974
Satwik Kambham <satwik.kambham@gmail.com>
2024-05-06 14:07:09 +0530

    added plugins

--- nixos/homeManagerModules/rofi.nix
+++ nixos/homeManagerModules/rofi.nix
@@ -4,5 +4,13 @@
     enable = true;
     package = pkgs.rofi-wayland;
     theme = ./files/spotlight.rasi;
+    extraConfig = {
+      modi = "drun,calc,window";
+    };
+    plugins = with pkgs; [
+      (rofi-calc.override {
+        rofi-unwrapped = rofi-wayland-unwrapped;
+      })
+    ];
   };
 }
