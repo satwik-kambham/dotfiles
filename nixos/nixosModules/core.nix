3f26b8398b1d10b1e04087215df5ae6f44a6dfe8
Satwik Kambham <satwik.kambham@gmail.com>
2024-05-10 21:13:38 +0530

    using grub

--- nixos/nixosModules/core.nix
+++ nixos/nixosModules/core.nix
@@ -2,18 +2,18 @@
 
 {
   # Bootloader
-  boot.loader.systemd-boot.enable = true;
-  # boot.loader.efi.canTouchEfiVariables = true;
-  # boot.loader.grub = {
-  #   enable = true;
-  #   device = "nodev";
-  #   efiSupport = true;
-  #   # useOSProber = true;
-  #   catppuccin = {
-  #     enable = true;
-  #     flavour = "mocha";
-  #   };
-  # };
+  # boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.grub = {
+    enable = true;
+    device = "nodev";
+    efiSupport = true;
+    # useOSProber = true;
+    catppuccin = {
+      enable = true;
+      flavour = "mocha";
+    };
+  };
 
   # Networking
   networking.networkmanager.enable = true;
