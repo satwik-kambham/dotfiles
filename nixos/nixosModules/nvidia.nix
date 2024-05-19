8cbd2649b20769cb5da2790f9ba2e2d348bb903a
Satwik Kambham <satwik.kambham@gmail.com>
2024-05-19 10:19:48 +0530

    Added VK_DRIVER_FILES variable

--- nixos/nixosModules/nvidia.nix
+++ nixos/nixosModules/nvidia.nix
@@ -4,6 +4,7 @@
   # Environmental variables for hyprland to work well on nvidia hardware
   environment.sessionVariables = {
     WLR_NO_HARDWARE_CURSORS = "1";
+    VK_DRIVER_FILES = "/run/opengl-driver/share/vulkan/icd.d/intel_icd.x86_64.json:/run/opengl-driver/share/vulkan/icd.d/nvidia_icd.x86_64.json";
   };
 
   # GPU Drivers and other nvidia related configs
