7929b8b5ee1b94a01cefb6f4b49c6e1236e466a0
Satwik Kambham <satwik.kambham@gmail.com>
2024-05-26 22:44:14 +0530

    using sddm

--- nixos/nixosModules/kde.nix
+++ nixos/nixosModules/kde.nix
@@ -1,9 +1,16 @@
 { pkgs, ... }:
 {
+  services.displayManager.sddm = {
+    enable = true;
+    theme = "catppuccin-mocha";
+  };
   services.desktopManager.plasma6.enable = true;
   programs.ssh.askPassword = pkgs.lib.mkForce "${pkgs.gnome.seahorse.out}/libexec/seahorse/ssh-askpass";
 
   environment.systemPackages = with pkgs; [
+    (catppuccin-sddm.override {
+      flavor = "mocha";
+    })
     (catppuccin-kde.override {
       flavour = [ "mocha" ];
       accents = [ "mauve" ];
