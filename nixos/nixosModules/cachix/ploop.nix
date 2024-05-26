8ecc18641168cac56f2e7c4d4fd0e4c123728576
Satwik Kambham <satwik.kambham@gmail.com>
2024-05-26 14:26:19 +0530

    added ploop cache

---
+++ nixos/nixosModules/cachix/ploop.nix
@@ -0,0 +1,12 @@
+{
+  nix = {
+    settings = {
+      substituters = [
+        "https://ploop.cachix.org"
+      ];
+      trusted-public-keys = [
+        "ploop.cachix.org-1:i6+Fqarsbf5swqH09RXOEDvxy7Wm7vbiIXu4A9HCg1g="
+      ];
+    };
+  };
+}
