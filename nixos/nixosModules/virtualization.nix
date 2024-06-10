e6c92bc4781955d876c291a1d22c6f55a13503bd
Satwik Kambham <satwik.kambham@gmail.com>
2024-06-10 22:09:10 +0530

    Added kvm options for osx-kvm

--- nixos/nixosModules/virtualization.nix
+++ nixos/nixosModules/virtualization.nix
@@ -10,4 +10,9 @@
   ];
   virtualisation.libvirtd.enable = true;
   users.users.satwik.extraGroups = [ "libvirtd" ];
+  boot.extraModprobeConfig = ''
+    options kvm_intel nested=1
+    options kvm_intel emulate_invalid_guest_state=0
+    options kvm ignore_msrs=1
+  '';
 }
