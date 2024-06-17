0f5dd840f1f1bc77440863c7b383574b9005ccde
Satwik Kambham <satwik.kambham@gmail.com>
2024-06-17 18:04:08 +0530

    added tailwind lsp to helix

--- nixos/homeManagerModules/helix.nix
+++ nixos/homeManagerModules/helix.nix
@@ -9,6 +9,25 @@
       editor.soft-wrap.enable = true;
       editor.color-modes = true;
     };
+    ignores = [
+      "!.gitignore"
+    ];
+    languages.language = [{
+      name = "html";
+      language-servers = [ "vscode-html-language-server" "tailwindcss-ls" ];
+    }
+    {
+      name = "css";
+      language-servers = [ "vscode-css-language-server" "tailwindcss-ls" ];
+    }
+    {
+      name = "tsx";
+      language-servers = [ "typescript-language-server" "tailwindcss-ls" ];
+    }
+    {
+      name = "jsx";
+      language-servers = [ "typescript-language-server" "tailwindcss-ls" ];
+    }];
     themes.catppuccin_mocha_transparent = {
       inherits = "catppuccin_mocha";
       "ui.background" = { };
