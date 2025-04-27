f34ea068723f7c721325860b556cdb33eb88b4fd
Satwik Kambham <satwik.kambham@gmail.com>
2025-04-27 11:46:06 +0530

    added plymouth

--- nixos/nixosModules/hyprland.nix
+++ nixos/nixosModules/hyprland.nix
@@ -4,6 +4,30 @@
   # Enable xserver
   services.xserver.enable = true;
 
+  # Plymouth
+  boot = {
+    # plymouth = {
+    #   enable = true;
+    #   theme = "circle_hud";
+    #   themePackages = with pkgs; [
+    #     (adi1090x-plymouth-themes.override {
+    #       selected_themes = [ "cuts" "circle_hud" ];
+    #     })
+    #   ];
+    # };
+
+    consoleLogLevel = 3;
+    initrd.verbose = false;
+    kernelParams = [
+      "quiet"
+      "splash"
+      "boot.shell_on_fail"
+      "udev.log_priority=3"
+      "rd.systemd.show_status=auto"
+    ];
+    loader.timeout = 0;
+  };
+
   # Enable hyprland
   programs.hyprland = {
     enable = true;
