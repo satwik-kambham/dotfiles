5cd753e2d7df400bffffde273609bd28c983bb4b
Satwik Kambham <satwik.kambham@gmail.com>
2025-10-07 19:35:24 +0530

    added ssh service

--- nixos/hosts/rpi/configuration.nix
+++ nixos/hosts/rpi/configuration.nix
@@ -164,7 +164,7 @@
     enable = true;
   };
 
-  # services.openssh.enable = true;
+  services.openssh.enable = true;
 
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
